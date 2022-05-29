db7e73be90ab7074c4763ff8fbb059a72047ecd4
Luis Ordonez <laordonez@unah.hn>
2022-05-28 20:07:47 -0600

    Tarea

--- Ejercicios/Tarea1/Calculadora.cpp
+++ Ejercicios/Tarea1/Calculadora.cpp
@@ -1,5 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    //Variables
+    double a = 0;
+    double b = 0;
+    int suma;
+    int resta;
+    int mult;
+    double div;
+    
+    //Ingreso de datos
+    cout << "Ingrese el valor de a: ";
+    cin >> a;
+
+    cout << endl;
+
+    cout << "Ingrese el valor de b: ";
+    cin >> b;
+
+    //Proceso
+    suma = a + b;
+    resta = a - b;
+    mult = a * b;
+    div = a / b;
+    cout << endl;
+
+    //Impresion de resultado
+    cout << "La suma de a + b es: " << suma << endl;
+    cout << "La resta de a - b es: " << resta << endl;
+    cout << "La multiplicacion de a * b es: " << mult << endl;
+    cout << "La division de a / b es: " << div << endl;
+
     return 0;
 }
