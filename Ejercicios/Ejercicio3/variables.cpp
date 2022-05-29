96fe1deae0e3b9aee735e8215260cb42311afe31
Luis Ordonez <laordonez@unah.hn>
2022-05-28 19:04:45 -0600

    Orden

---
+++ Ejercicios/Ejercicio3/variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeroEntero;
+    numeroEntero = 15;
+
+    cout << numeroEntero;
+
+    numeroEntero = 18;
+
+    cout << numeroEntero;
+
+    return 0;
+}
