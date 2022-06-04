dfcad9e4f6a8c29f15734e1fb66e23c26710484b
Luis Ordonez <laordonez@unah.hn>
2022-06-04 15:59:30 -0600

    Tarea2

---
+++ Ejercicios/Tarea2/FactEx.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // Datos de entrada
+    
+    double subtotal = 0;
+    double total = 0;
+    double impuesto = 0.15;
+    int descuento;
+    double calculoDesc = 0;
+    double calculoImp = 0;
+    char exenta;
+
+    cout << "Ingrese el subtotal de la factura: ";
+    cin >> subtotal;
+
+    cout << "Ingrese el descuento (0, 10, 15, 20): ";
+    cin >> descuento;
+
+    cout << "Es factura exenta? Ingrese S/N: ";
+    cin >> exenta;
+
+    
+    
+    // Proceso 1
+
+    if (exenta == 'S' || exenta == 's')
+    {
+    calculoDesc = (subtotal * descuento) / 100; 
+    total = subtotal - calculoDesc;
+
+    // Salida 1
+
+    cout << endl;
+    cout << "Total a pagar es: " << total;
+    }
+    
+    // Proceso 2
+
+    else if (exenta == 'N' || exenta == 'n')
+    {   
+    calculoDesc = (subtotal * descuento) / 100; 
+    calculoImp = (subtotal - calculoDesc) * impuesto;
+    total = subtotal - calculoDesc + calculoImp;
+    
+    // Salida 2
+
+    cout << endl;
+    cout << "Total a pagar es: " << total;
+    }
+
+    //Error
+
+    else
+
+    {
+    cout << "Error!!! Letra ingresada invalida.";
+    }
+    
+    return 0;
+}
