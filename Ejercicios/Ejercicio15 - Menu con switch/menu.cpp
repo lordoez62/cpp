38a4959da3740f6b18b5ed9364f69e3ae8e0f59f
Luis Ordonez <laordonez@unah.hn>
2022-06-26 05:41:00 -0600

    menu con swithc

--- Ejercicios/Ejercicio15 - Menu con switch/menu.cpp
+++ Ejercicios/Ejercicio15 - Menu con switch/menu.cpp
@@ -6,6 +6,8 @@ int main(int argc, char const *argv[])
 {
     int opcion = 0;
 
+    
+
     while (true) {
         cout << "*****" << endl;
         cout << "Menu" << endl;
@@ -18,24 +20,34 @@ int main(int argc, char const *argv[])
         cout << "Ingrese una opcion: ";
         cin >> opcion;
 
-        if (opcion == 1) 
-        {
-            system("cls");
-            cout << "Ingreso al menu de Cafe y Granitas" << endl;
-            system("pause");
-        }
-
-        if (opcion == 2)
-        {
-            system("cls");
-            cout << "Ingreso al menu de reposteria" << endl;
-            system("pause");
-        }
-
+        
         if (opcion == 0)
         {
             break;
         }        
+
+        switch (opcion)
+            {
+            case 1: 
+                {
+                    system("cls");
+                    cout << "Ingreso al menu de Cafe y Granitas" << endl;
+                    system("pause");      
+                    break;
+                }
+
+            case 2:
+                {
+                    system("cls");
+                    cout << "Ingreso al menu de reposteria" << endl;
+                    system("pause");
+                    break;
+                }
+            default:
+                cout << "Ingrese una opcion valida (0,1,2)" << endl;
+                system("pause");
+                break;
+            }
 
     }
 
