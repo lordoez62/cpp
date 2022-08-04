f77ba0291102165c9d4fe0a413e09116871f74c4
Luis Ordonez <laordonez@unah.hn>
2022-08-04 10:42:12 -0600

    Funcioes

---
+++ Ejercicios/Ejercicio19 - Funciones/funciones.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int sumar() {
+    return 5 + 7;
+}
+
+int main(int argc, char const *argv[])
+{
+  
+  int resultado = sumar();
+
+  cout<< resultado;
+
+  return 0;
+
+}
