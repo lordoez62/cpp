7a3f29650e4b5cf14be0750baafe98edf175f92a
Luis Ordonez <laordonez@unah.hn>
2022-08-04 10:56:26 -0600

    mas funciones

---
+++ Ejercicios/Ejercicio20 - Funciones/funciones.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int sumar (int a, int b){
+    return a + b;
+}
+
+int main(int argc, char const *argv[])
+{
+
+    system("cls");
+    cout << sumar (5,7);
+    cout<< endl;
+    cout << sumar (55,7);
+    cout<< endl;
+    cout << sumar (5,17);
+    cout<< endl;
+    cout << sumar (25,7);
+    cout<< endl;
+
+    return 0;
+
+}
