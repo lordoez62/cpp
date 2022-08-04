2cc18dc4f659af0a933fbc9da79ec9ea95decd27
Luis Ordonez <laordonez@unah.hn>
2022-08-04 10:47:50 -0600

    Intro

--- Ejercicios/Ejercicio19 - Funciones/funciones.cpp
+++ Ejercicios/Ejercicio19 - Funciones/funciones.cpp
@@ -7,12 +7,22 @@ int sumar() {
     return 5 + 7;
 }
 
+string nombreCompleto(){
+    string nombre = "Juan";
+    string apellido = "Perez";
+
+    return nombre + " " + apellido;
+}
+
 int main(int argc, char const *argv[])
 {
   
   int resultado = sumar();
+  string nombreyApellido = nombreCompleto ();
 
   cout<< resultado;
+  cout<< endl;
+  cout<< nombreyApellido;
 
   return 0;
 
